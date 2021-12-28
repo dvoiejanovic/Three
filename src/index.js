3322bb33d6ced4a4a6585ada0bf5ba88e5f3f15f
Ivo Dejanovic <ivo.dejanovic@productive.io>
2021-12-28 23:55:17 +0100

    boxy box

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
-import './style.css';
+import { PerspectiveCamera, Scene, BoxGeometry, WebGLRenderer, Mesh, MeshBasicMaterial } from 'three';
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0,1, 1000);
-const renderer = new WebGLRenderer();
+let camera, scene, renderer;
+let geometry, material, mesh;
 
-renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
+init();
+
+function init() {
+
+	camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new Scene();
+
+	geometry = new BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new MeshBasicMaterial({
+    color: 'cyan'
+  });
+
+	mesh = new Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animation );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
+
