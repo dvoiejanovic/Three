5e74354b6af35c4f117382758f7f00c835736c0f
Ivo Dejanovic <ivo.dejanovic@productive.io>
2021-12-29 20:53:04 +0100

    dat gui magic

--- src/index.js
+++ src/index.js
@@ -1,38 +1,78 @@
-import { PerspectiveCamera, Scene, BoxGeometry, WebGLRenderer, Mesh, MeshBasicMaterial } from 'three';
+import {
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  Mesh,
+  PlaneGeometry,
+  DoubleSide,
+  DirectionalLight,
+  MeshPhongMaterial,
+  FlatShading,
+} from "three";
+import * as dat from "dat.gui";
 
-let camera, scene, renderer;
-let geometry, material, mesh;
+import "./style.css";
 
 init();
 
 function init() {
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    1000
+  );
+  camera.position.z = 5;
 
-	camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+  const scene = new Scene();
 
-	scene = new Scene();
-
-	geometry = new BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new MeshBasicMaterial({
-    color: 'cyan'
+  const planeGeometry = new PlaneGeometry(10, 10, 10, 10);
+  const planeMaterial = new MeshPhongMaterial({
+    color: "red",
+    side: DoubleSide,
+    flatShading: FlatShading,
   });
 
-	mesh = new Mesh( geometry, material );
-	scene.add( mesh );
+  const planeMesh = new Mesh(planeGeometry, planeMaterial);
+  scene.add(planeMesh);
 
-	renderer = new WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-	document.body.appendChild( renderer.domElement );
+  const light = new DirectionalLight(0xffffff, 1);
+  light.position.set(0, 0, 1);
+  scene.add(light);
 
+  const gui = new dat.GUI();
+  const world = {
+    plane: {
+      width: 10,
+      height: 10,
+      widthSegments: 10,
+      heightSegments: 10,
+    },
+  };
+
+	const generatePlane = () => {
+			planeMesh.geometry.dispose();
+			const {width, height, widthSegments, heightSegments} = world.plane;
+			planeMesh.geometry = new PlaneGeometry(width, height, widthSegments, heightSegments);
+	
+			const vertices = planeMesh.geometry.attributes.position.array;
+			for (let i = 0; i <= vertices.length; i += 3) {
+				const z = vertices[i + 2];
+				vertices[i + 2] = z + Math.random();
+		};
+	}
+	gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+	gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+	gui.add(world.plane, "widthSegments", 1, 50).onChange(generatePlane);
+	gui.add(world.plane, "heightSegments", 1, 50).onChange(generatePlane);
+
+
+  const renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  document.body.appendChild(renderer.domElement);
+
+  function animation(time) {
+    renderer.render(scene, camera);
+  }
 }
-
-function animation( time ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
-}
-
